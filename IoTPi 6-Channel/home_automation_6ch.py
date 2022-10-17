7fb5cbbe95f3ab8ad5da8e3b5f9f510834b86be9
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-10-17 11:31:17 +0530

    Update home_automation_6ch.py

--- IoTPi 6-Channel/home_automation_6ch.py
+++ IoTPi 6-Channel/home_automation_6ch.py
@@ -1,3 +1,8 @@
+'''
+This is the Example code of home automation for conntrolling 4-ch relays by using Android App
+This code is Devolop by Sb-componetns 
+'''
+
 from IoTPi import Wifi
 from IoTPi import Relay_6ch,Status_led
 import time
